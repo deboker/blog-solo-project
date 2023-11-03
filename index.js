622cbf2f89891653e82d28dfd6ef8944d90a50ab
deboker <117391017+deboker@users.noreply.github.com>
2023-11-03 22:55:35 +0100

    changes

--- index.js
+++ index.js
@@ -1,19 +1,32 @@
-function myFunction() {
-    let links = document.getElementById("my-links");
-    if (links.style.display === "block") {
-      links.style.display = "none";
-    } else {
-      links.style.display = "block";
-    }
+// Define the toggleMenu function to handle the navigation menu
+function toggleMenu() {
+  const links = document.getElementById("my-links");
+  if (links.style.display === "block") {
+    links.style.display = "none";
+  } else {
+    links.style.display = "block";
   }
-  
-  const viewMoreBtn = document.getElementById("view-more")
-  let news = 
-  
-  viewMoreBtn.addEventListener("click", function(){
-      displayNews()
-  })
-  
-  function displayNews() {
-      Array.from(document.getElementsByClassName("news-recent")).forEach(element => element.classList.add('display'))
+}
+
+// You can define other functions for specific pages here
+
+// Example function for a specific page
+function pageSpecificFunction() {
+  // Add your code here for a specific page
+}
+
+// Call page-specific functions when the page loads
+document.addEventListener("DOMContentLoaded", function () {
+  // Check the current page and call the relevant function
+  if (window.location.pathname === "/index.html") {
+    // Call the function for the index.html page
+    pageSpecificFunction();
   }
+});
+
+// You can add more functions as needed for other pages
+
+// Example function for another specific page
+function anotherPageSpecificFunction() {
+  // Add your code here for another specific page
+}
